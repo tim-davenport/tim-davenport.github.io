887ad0a8c35175a989d7e2169ba1448e0474106a
Tim Davenport <Tim.Davenport@skybettingandgaming.com>
2022-01-17 14:38:07 +0000

    Implement menu overlay

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
+function showMenu() {
+  document.getElementById("primaryNav").style.width = "100%";
 }
-const x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu; 
+function closeMenu() {
+  document.getElementById("primaryNav").style.width = "0";
+}
+
+const openMenuBtn = document.getElementById("openMenuBtn");
+openMenuBtn.onclick = showMenu;
+
+const closeMenuBtn = document.getElementById("closeMenuBtn");
+console.dir(closeMenuBtn);
+closeMenuBtn.onclick = closeMenu;
